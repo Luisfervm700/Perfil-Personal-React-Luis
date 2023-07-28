17fc909ea57d93efe5bc9d064edbfa78f4e0823e
Luis Fernando <luisfernandovm700@gmail.com>
2023-07-27 20:45:26 -0500

    Se organizo de acuerdo al Doc

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,22 @@ import img from './components/img/Luis_Fernando_Velásquez_Monsalve.jpg.jpg'; //
 
 import {Header} from './components/Header';
 import {Profile} from './components/Profile';
-import {Certificates} from './components/Certificates';
 import {AcademicBackground} from './components/AcademicBackground';
 import {Experience} from './components/Experience';
 import {Skills} from './components/Skills';
 import {Interests} from './components/Interests';
 import {Languages} from './components/Languages';
-import {Social} from './components/Social';
 
 function App() {
   const profileData = {
-    name: "Luis Fernando Velásquez Monsalve",
-    profession: "FrontEnd Developer",
-    address: "Filandia, Colombia.",
-    email: "luisfernandovm700@gmail.com",
-    website: "https://www.xtrafondos.com/",
-    phone: "3136623627",
-    avatar: img,
-    profile: "Soy un programador apasionado con experiencia en el desarrollo de software y soluciones innovadoras. Mi objetivo principal es crear aplicaciones de alta calidad que cumplan con las necesidades y expectativas de los usuarios. Con un enfoque en el diseño limpio y la funcionalidad eficiente, busco constantemente mejorar mis habilidades y mantenerme actualizado con las últimas tecnologías y tendencias en el mundo del desarrollo de software.",
-    certificate: [
-      {
-        date: "11 de Diciembre de 2020",
-        description: "Grado de bachiller",
-        institution: "Liceo Andino De La Santicima Trinidad",
-        name: "Diploma",
-      }
-    ],
+      name: "Luis Fernando Velásquez Monsalve",
+      profession: "FrontEnd Developer",
+      address: "Filandia, Colombia.",
+      email: "luisfernandovm700@gmail.com",
+      website: "https://www.xtrafondos.com/",
+      phone: "3136623627",
+      avatar: img,
+      profile: "Soy un programador apasionado con experiencia en el desarrollo de software y soluciones innovadoras. Mi objetivo principal es crear aplicaciones de alta calidad que cumplan con las necesidades y expectativas de los usuarios. Con un enfoque en el diseño limpio y la funcionalidad eficiente, busco constantemente mejorar mis habilidades y mantenerme actualizado con las últimas tecnologías y tendencias en el mundo del desarrollo de software.",
     academic: [
       {
         degree: "Tecnologo en Analisi y Desarrollo de Software.",
@@ -54,7 +44,7 @@ function App() {
         percentage: "70%"
       }
     ],
-    interest: ["React", "develop", "backend", "frontend"],
+    interest: ["backend", "frontend"],
     languages: [
       {
         name: "Spanish",
@@ -64,18 +54,9 @@ function App() {
         name: "English",
         percentage: "45%"
       }
-    ],
-    social: [
-      {
-        name: "github",
-        url: "https://github.com/Luisfervm700"
-      },
-      {
-        name: "linkedin",
-        url: "https://www.linkedin.com/"
-      }
     ]
   };
+
 
   return (
     <div className="container mx-auto p-4 bg-blue-200">
@@ -95,12 +76,10 @@ function App() {
         <div className="w-1/2 pr-4">
           <AcademicBackground academic={profileData.academic} />
           <Interests interests={profileData.interest} />
-          <Social social={profileData.social} />
         </div>
         
         <div className="w-1/2 pr-4">
           <Languages languages={profileData.languages} />
-          <Certificates certificates={profileData.certificate} />
           <Skills skills={profileData.skills} />
         </div>
       </div>
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Interests({ interests }) {
   return (
-    <div className="bg-green-200 rounded-lg shadow p-4 mb-4">
+    <div className="bg-green-200 rounded-lg shadow p-5 mb-4">
       <h2 className="text-xl font-bold mb-2">Interests</h2>
       <ul className="list-disc ml-6">
         {interests.map((interest, index) => (
